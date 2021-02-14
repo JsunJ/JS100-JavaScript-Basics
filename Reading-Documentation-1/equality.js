fdaadedc81e0ad1d45ee64f858e07b289859a9c1
Jason Jones <JsunJ.Jones@gmail.com>
2021-02-13 23:04:34 -0500

    Add remaining reading doc 1 exercises

---
+++ Reading-Documentation-1/equality.js
@@ -0,0 +1,5 @@
+'8' == 8; // returns true
+'8' === 8; // returns false
+
+// == coerces the string to a number before comparing
+// === does not perform type conversion before comparing
---
+++ Reading-Documentation-1/property-vs-method.js
@@ -0,0 +1,5 @@
+let trees = ['birch', 'pine', 'sequoia', 'palm tree'];
+
+trees[trees.length - 1]; // returns 'palm tree'
+trees.pop(); // returns 'palm tree'
+trees[trees.length - 1]; // returns 'sequoia'
