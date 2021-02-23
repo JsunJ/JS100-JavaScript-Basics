f941b445dd592085c1db6ae6d11b6d4669780998
Jason Jones <JsunJ.Jones@gmail.com>
2021-02-23 17:26:34 -0500

    Add objects exercises

---
+++ Objects/and-vice-versa.js
@@ -0,0 +1,16 @@
+let nestedArray = [['title', 'Duke'], ['name', 'Nukem'], ['age', 33]];
+
+// let person = Object.fromEntries(nestedArray);
+
+let person = {};
+
+for (let i = 0; i < nestedArray.length; i++) {
+  let pair = nestedArray[i];
+
+  person[pair[0]] = pair[1];
+}
+
+console.log(person);
+
+// Expected output:
+// { title: 'Duke', name: 'Nukem', age: 33 }
